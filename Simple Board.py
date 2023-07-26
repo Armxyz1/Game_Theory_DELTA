dfd92cba3558c47aa1930620df94d970db109edf
spurjhattam2207 <133047073+spurjhattam2207@users.noreply.github.com>
2023-07-26 23:45:28 +0530

    Update Simple Board.py

--- Simple Board.py
+++ Simple Board.py
@@ -93,8 +93,24 @@ def minimax(board, depth, turn):
     """
     pass
 def evaluate(board,turn):
-    #TODO: 
-    pass
+    for i in range(3):
+        if board[i][0]==board[i][1] and board[i][1]==board[i][2]:
+            if board[i][0]=='x':
+                return 10;
+            elif board[i][0]=='o':
+                return -10;
+        if board[0][i]==board[1][i] and board[1][i]==board[2][i]:
+            if board[0][i] == 'x':
+                return 10;
+            elif board[0][i] == 'o':
+                return -10;
+    if board[0][0]==board[1][1] and board[1][1]==board[2][2]:
+        if board[0][0] == 'x':
+            return 10;
+        elif board[0][0] == 'o':
+            return -10;
+    else:
+        return 0;
 print("Welcome to Tic Tac Toe!")
 print("This is a 3x3 board. Enter the coordinates of the square you want to place your X or O in.")
 print("The first player to get three in a row wins!")
